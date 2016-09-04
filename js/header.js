a94957086405bff61f8d65beec7230bec98b4324
Splice09 <jpf0005@gmail.com>
2016-09-04 17:46:48 -0500

    Removing javascript and coloring header titles much more intelligently.

--- js/header.js
+++ js/header.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
     //Calls the getData function when the page is ready.
-    document.getElementById("header").style.color = "#e1d6ce";
-    document.getElementById("colorHeader").style.color = "#6d1612";
+   
 });
