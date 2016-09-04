09c85e99b2a14a34e2c694d0b9a03e12e2638d3d
Splice09 <jpf0005@gmail.com>
2016-09-04 14:51:31 -0500

    changing javascript to use id instead of class.

--- js/header.js
+++ js/header.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     //Calls the getData function when the page is ready.
-    $('.header').style.color = "#e1d6ce";
+    document.getElementById("header").style.color = "#e1d6ce";
     document.getElementById("colorHeader").style.color = "#6d1612";
 });
