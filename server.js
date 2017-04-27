096e28f3939057ba084269187be7784583f6987d
Splice09 <jpf0005@gmail.com>
2017-04-26 21:00:21 -0500

    trying a smoother image fetch in server code because wine not

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.use('/public/', function(request, response){
                             response.write(file, "binary");
                             response.end();
                         }
-                        else if(fullPath == "/app/photos/New_Logo_Gmail.svg"){
+                        else if(fullPath == "/app/photos/*"){
                             console.log("&&&&&&& DEFINITELY PICKING UP THAT PHOTO PATH &&&&&&&&");
                             response.writeHeader(200, {"Content-Type": "image/svg+xml"});
                             response.write(file, "binary");
