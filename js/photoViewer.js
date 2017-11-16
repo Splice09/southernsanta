d5ea26edc30638c0af87af95289ecab086321289
Splice09 <jpf0005@gmail.com>
2017-11-16 18:01:45 -0500

    Added extra photos

--- js/photoViewer.js
+++ js/photoViewer.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
     var photoUrls = [
-        'http://i.imgur.com/jjt13WD.jpg'
-        ,'http://i.imgur.com/O4bkw5q.jpg'
-        ,'http://i.imgur.com/Ur03C5l.jpg'
-        ,'http://i.imgur.com/E7zVb9o.jpg'
-        ,'http://i.imgur.com/rAScQtY.jpg'
-        ,'http://i.imgur.com/XHisZYy.jpg'
-        ,'http://i.imgur.com/cwgwtmV.jpg'
-        ,'http://i.imgur.com/PJ3Upzg.jpg'
-        ,'http://i.imgur.com/Xlxvo1v.jpg'
-        ,'http://i.imgur.com/LH4AZew.jpg'
-        ,'http://i.imgur.com/9O6fLlH.jpg'
-        ,'http://i.imgur.com/tdSUQ8c.jpg'
+        'https://i.imgur.com/jjt13WD.jpg'
+        ,'https://i.imgur.com/O4bkw5q.jpg'
+        ,'https://i.imgur.com/Ur03C5l.jpg'
+        ,'https://i.imgur.com/E7zVb9o.jpg'
+        ,'https://i.imgur.com/rAScQtY.jpg'
+        ,'https://i.imgur.com/XHisZYy.jpg'
+        ,'https://i.imgur.com/cwgwtmV.jpg'
+        ,'https://i.imgur.com/PJ3Upzg.jpg'
+        ,'https://i.imgur.com/Xlxvo1v.jpg'
+        ,'https://i.imgur.com/LH4AZew.jpg'
+        ,'https://i.imgur.com/9O6fLlH.jpg'
+        ,'https://i.imgur.com/tdSUQ8c.jpg'
+        ,'https://i.imgur.com/J9BJ04x.jpg'
+        ,'https://i.imgur.com/jyAb2vV.jpg'
     ]
     var tinyGif = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
     
