a48431d85ed1d017d7a87b5394ceacb8fb50683f
Patrick Fleming <pfleming@hylaine.com>
2022-12-16 12:50:40 -0500

    correcting bugs on 2022 update

--- js/galleryViewer.js
+++ js/galleryViewer.js
@@ -4,15 +4,45 @@ $(document).ready(function(){
         $('.numberLink').css('color', 'white');
         $(this).css('color', '#d00008');
         var galleryBoxId = $(this).data('value');
-        if (galleryBoxId == 18){
+        if (galleryBoxId == 22){
+            $('#gb21').hide();
+            $('#gb19').hide();
+            $('#gb18').hide();
+            $('#gb17').hide();
+            $('#gb16').hide();
+        }
+        else if (galleryBoxId == 21){
+            $('#gb22').hide();
+            $('#gb19').hide();
+            $('#gb18').hide();
+            $('#gb17').hide();
+            $('#gb16').hide();
+        }
+        else if (galleryBoxId == 19){
+            $('#gb22').hide();
+            $('#gb21').hide();
+            $('#gb18').hide();
+            $('#gb17').hide();
+            $('#gb16').hide();
+        }
+        else if (galleryBoxId == 18){
+            $('#gb22').hide();
+            $('#gb21').hide();
+            $('#gb19').hide();
             $('#gb17').hide();
             $('#gb16').hide();
         }
         else if (galleryBoxId == 17){
+            $('#gb22').hide();
+            $('#gb21').hide();
+            $('#gb19').hide();
             $('#gb18').hide();
             $('#gb16').hide();
         }
         else{
+            $('#gb22').hide();
+            $('#gb21').hide();
+            $('#gb19').hide();
             $('#gb18').hide();
             $('#gb17').hide();
         }
